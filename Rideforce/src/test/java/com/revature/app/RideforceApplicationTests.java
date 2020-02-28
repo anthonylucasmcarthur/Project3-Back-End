e479d8d1af311a40f782275fad0cefb5014a3f4b
dlazzari <dlazzari@my.hpu.edu>
2020-02-28 10:57:28 -0500

    office service tests work

--- Rideforce/src/test/java/com/revature/app/RideforceApplicationTests.java
+++ Rideforce/src/test/java/com/revature/app/RideforceApplicationTests.java
@@ -23,4 +23,29 @@ class RideforceApplicationTests {
 	}
 			
 	}
-}
+	
+	@Test //works
+	void getOfficeById() {
+		Office office = os.getOfficeById(2); 
+		System.out.println(office);
+		
+		
+	}
+	
+	@Test //works
+	void addOffice() {
+		Office office = new Office(0, "123 hong kong drive"); 
+		os.addOffice(office); 
+		System.out.println(office);
+	}
+	
+	@Test //works 
+	void updateOffice() {
+		Office office = os.getOfficeById(5);
+		office.setOffice_address("123 belaire dr");
+		os.updateOffice(office); 
+		System.out.println(office);
+	}
+			
+	}
+
