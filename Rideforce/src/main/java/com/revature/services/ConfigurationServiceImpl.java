160ebd3843957f6680bad52d14f6439c2747b63c
Katie Hairston <kth5sf@virginia.edu>
2020-02-27 21:39:57 -0500

    repo

---
+++ Rideforce/src/main/java/com/revature/services/ConfigurationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.revature.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service
+public class ConfigurationServiceImpl implements ConfigurationService {
+
+	@Autowired
+	ConfigurationRepo cr;
+	
+	@Override
+	public String getConfigurationByLabel(String label) {
+		Configuration c = cr.findByLabel(label);
+		return c.getValue();
+	}
+
+}
