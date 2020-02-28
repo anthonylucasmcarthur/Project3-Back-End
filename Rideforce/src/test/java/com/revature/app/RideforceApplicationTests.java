6792562f4f295e5fa8ad9e1fa9c2bc58f30a966f
dlazzari <dlazzari@my.hpu.edu>
2020-02-28 10:19:56 -0500

    a service test works

--- Rideforce/src/test/java/com/revature/app/RideforceApplicationTests.java
+++ Rideforce/src/test/java/com/revature/app/RideforceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.revature.app;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.revature.entities.Office;
+import com.revature.services.OfficeService;
 
 @SpringBootTest
 class RideforceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired 
+	OfficeService os; 
+	
+	@Test //works 
+	void getOffices() {
+		List<Office> offices = os.getOffices(); 
+			for (Office o : offices) {
+			System.out.println(o);
 	}
-
+			
+	}
 }
