e312a8440bc35cb544f77e75bbeee4ea736abd37
inamjr <ikeogu56@gmail.com>
2020-02-28 09:35:36 -0600

    new

--- Rideforce/src/main/java/com/revature/aspects/LogIt.java
+++
@@ -1,5 +0,0 @@
-package com.revature.aspects;
-
-public @interface LogIt {
-
-}
--- Rideforce/src/main/java/com/revature/services/ConfigurationService.java
+++
@@ -1,7 +0,0 @@
-package com.revature.services;
-
-public interface ConfigurationService {
-
-	public String getConfigurationByLabel(String label);
-	
-}
